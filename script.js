e2a70bfe00dad362b42d35958b35940f6a03e87f
Catalin Gavrila <cata_bb92@yahoo.com>
2021-05-31 12:31:20 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+
+/* Responsive top-nav start /*
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+document.getElementById("menuButton").onclick = function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
